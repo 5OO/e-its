96c210facc841e6ac1ee8a459b79f3b92b5f8445
5OO <git.hub@myrk.eu>
2024-06-11 16:02:52 +0300

    Intial commit

---
+++ src/main/java/org/ria/eits/EitsApplication.java
@@ -0,0 +1,13 @@
+package org.ria.eits;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EitsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EitsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/ria/eits/EitsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ria.eits;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EitsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
