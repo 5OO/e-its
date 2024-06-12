40c0a5ebea68c97dbc2daf2a083b72e8f63bd44b
5OO <git.hub@myrk.eu>
2024-06-12 15:07:18 +0300

    Add header to index page and enhance Article model
    A new header has been added to the index.html for better labeling. Also, the Article model has been enhanced with multiple new fields. The model is now ignoring unknown properties and also includes an "id" field and a "child_objects" field and other potential fields have been commented out for further consideration.

--- src/main/java/org/ria/eits/model/Article.java
+++ src/main/java/org/ria/eits/model/Article.java
@@ -1,12 +1,32 @@
 package org.ria.eits.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 import java.util.List;
-
+@JsonIgnoreProperties(ignoreUnknown = true)
 @Data
 public class Article {
     private String title;
     private String content;
+
+    @JsonProperty("child_objects")
     private List<Article> childObjects;
+
+//    private boolean parentPage;
+//    private boolean searchTopLevelPage;
+//    private String layout;
+//    private String date;
+//    private String permalink;
+   private String id;
+//    private String endDate;
+//    private String startDate;
+//
+//    @JsonProperty("media_order")
+//    private String mediaOrder;
+//
+//    private String icon;
+//    private String category;
+//    private List<String> assignee;
 }
