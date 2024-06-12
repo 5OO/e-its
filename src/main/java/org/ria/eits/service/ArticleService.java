6468cbd8a66213691a3809e35a5ded8c7465b8d7
5OO <git.hub@myrk.eu>
2024-06-12 14:48:00 +0300

    Add Jackson dependencies and refactor fetchData method
    Added Jackson dependencies in pom.xml for better JSON handling. Refactored the fetchData method in the ArticleService class to use Jackson's JsonParser and ObjectMapper instead of RestTemplate to fetch and process the JSON data.

--- src/main/java/org/ria/eits/service/ArticleService.java
+++ src/main/java/org/ria/eits/service/ArticleService.java
@@ -1,12 +1,16 @@
 package org.ria.eits.service;
 
+import com.fasterxml.jackson.core.JsonFactory;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.JsonToken;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.ria.eits.model.Article;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
+import java.io.IOException;
+import java.net.URL;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 @Slf4j
@@ -15,34 +19,44 @@ public class ArticleService {
     private static final String RIA_EITS_API_2_ARTICLE_2024 = "https://eits.ria.ee/api/2/article/2024";
 
     public List<List<String>> fetchData() {
-        RestTemplate restTemplate = new RestTemplate();
-        Article[] articles = restTemplate.getForObject(RIA_EITS_API_2_ARTICLE_2024, Article[].class);
         List<List<String>> data = new ArrayList<>();
-        if (articles != null) {
-            log.debug("articles length {} bytes ", articles.length);
-            log.debug(Arrays.toString(articles));
-            extractTitles(articles, data, List.of("3.2 Põhimeetmed", "3.3 Standardmeetmed", "3.4 Kõrgmeetmed"));
-        } else {
-            log.warn("No articles retrieved from the API.");
-        }
+        try {
+            JsonFactory jsonFactory = new JsonFactory();
+            URL url = new URL(RIA_EITS_API_2_ARTICLE_2024);
+            try (JsonParser jsonParser = jsonFactory.createParser(url)) {
+                if (jsonParser.nextToken() == JsonToken.START_ARRAY) {
+                    ObjectMapper objectMapper = new ObjectMapper();
+                    while (jsonParser.nextToken() != JsonToken.END_ARRAY) {
+                        Article article = objectMapper.readValue(jsonParser, Article.class);
+                        log.debug("Processing article: {}", article.getId());
+                        extractDataFromArticle(article, data);
+                        log.debug("Processing completed");
 
+                    }
+                }
+            }
+        } catch (IOException e) {
+            log.error("Error fetching or parsing JSON data", e);
+        }
         return data;
     }
 
-    private void extractTitles(Article[] articles, List<List<String>> data, List<String> sections) {
-        for (Article article : articles) {
-            if (sections.contains(article.getTitle()) && article.getChildObjects() != null) {
-                    for (Article child : article.getChildObjects()) {
-                        List<String> row = new ArrayList<>();
-                        row.add(article.getTitle());
-                        row.add(child.getTitle());
-                        row.add(child.getContent());
-                        data.add(row);
-                    }
-                }
-
+    private void extractDataFromArticle(Article article, List<List<String>> data) {
+        if (article.getTitle().matches("3\\.2.*") || article.getTitle().matches("3\\.3.*") || article.getTitle().matches("3\\.4.*")) {
             if (article.getChildObjects() != null) {
-                extractTitles(article.getChildObjects().toArray(new Article[0]), data, sections);
+                for (Article child : article.getChildObjects()) {
+                    List<String> row = new ArrayList<>();
+                    row.add(article.getTitle());
+                    row.add(child.getTitle());
+                    row.add(child.getContent());
+                    data.add(row);
+                }
+            }
+        }
+
+        if (article.getChildObjects() != null) {
+            for (Article child : article.getChildObjects()) {
+                extractDataFromArticle(child, data);
             }
         }
     }
