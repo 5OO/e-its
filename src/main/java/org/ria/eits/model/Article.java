7a4cefca3546fccb5d0ef4d1853c46337e46b1c9
5OO <git.hub@myrk.eu>
2024-06-12 16:00:18 +0300

    Remove unused fields from Article model
    The changes from this commit include the clean-up of the Article model.

--- src/main/java/org/ria/eits/model/Article.java
+++ src/main/java/org/ria/eits/model/Article.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 import java.util.List;
+
 @JsonIgnoreProperties(ignoreUnknown = true)
 @Data
 public class Article {
@@ -14,19 +15,5 @@ public class Article {
     @JsonProperty("child_objects")
     private List<Article> childObjects;
 
-//    private boolean parentPage;
-//    private boolean searchTopLevelPage;
-//    private String layout;
-//    private String date;
-//    private String permalink;
-   private String id;
-//    private String endDate;
-//    private String startDate;
-//
-//    @JsonProperty("media_order")
-//    private String mediaOrder;
-//
-//    private String icon;
-//    private String category;
-//    private List<String> assignee;
+    private String id;
 }
